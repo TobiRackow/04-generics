c75e403c86257220970322080a088070dd486bfc
Tobi Rackow <bowser96@gmx.net>
2017-10-25 10:36:08 +0200

    Aufgabe 1 fertig

--- src/main/java/de/fhro/inf/prg3/a04/collections/SimpleList.java
+++ src/main/java/de/fhro/inf/prg3/a04/collections/SimpleList.java
@@ -1,10 +1,10 @@
 package de.fhro.inf.prg3.a04.collections;
 
-public interface SimpleList extends Iterable {
+public interface SimpleList<T> extends Iterable<T> {
 	/**
 	 * Add a given object to the back of the list.
 	 */
-	void add(Object o);
+	void add(T o);
 
 	/**
 	 * @return current size of the list
@@ -15,5 +15,6 @@ public interface SimpleList extends Iterable {
 	 * Generate a new list using the given filter instance.
 	 * @return a new, filtered list
 	 */
-	SimpleList filter(SimpleFilter filter);
+	void addDefault();
+	SimpleList<T> filter(SimpleFilter<T> filter);
 }
